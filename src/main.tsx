9121eb9e0a4630c3231386ec93429236a78f31c8
charleshong <qcsjin8@gamil.com>
2024-10-28 19:52:48 +0800

    dev

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ import './styles/dark.less'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,36 +4,23 @@ import { createBrowserRouter, Navigate, type RouteObject } from "react-router-do
 import Login from "@/pages/Login/index";
 
 // * 导入所有router
-const metaRouters: RouteObject = import.meta.glob("./modules/*.tsx");
+const metaRouters: any = import.meta.glob("./modules/*.tsx", { eager: true });
 
-// * 处理路由
-const routerArray: RouteObject[] = [];
+let routeModuleList: RouteObject[] = []
 
-async function loadRouters(): Promise<void> {
-    try {
-        // 确保类型正确，metaRouters 是一个包含 Promise 的对象
-        const routeModules = await Promise.all(
-            Object.values(metaRouters).map((importFn) => importFn())
-        );
-
-        routeModules.forEach((module) => {
-            // 检查 module 是否包含 default 属性，并确保 default 是 RouteObject 或 RouteObject[]
-            if (module && module.default) {
-                if (Array.isArray(module.default)) {
-                    routerArray.push(...module.default);
-                } else {
-                    routerArray.push(module.default);
-                }
-            }
-        });
-    } catch (error) {
-        console.error("Failed to load route modules:", error);
+Object.values(metaRouters).forEach((module: any) => {
+    // 检查 module 是否包含 default 属性，并确保 default 是 RouteObject 或 RouteObject[]
+    if (module && module.default) {
+        if (Array.isArray(module.default)) {
+            routeModuleList.push(...module.default);
+        } else {
+            routeModuleList.push(module.default);
+        }
     }
-}
+});
 
 
-// * 加载路由
-await loadRouters();
+
 
 const routes: RouteObject[] = [
     {
@@ -44,7 +31,7 @@ const routes: RouteObject[] = [
         path: "/login",
         element: <Login />,
     },
-    ...routerArray,
+    ...routeModuleList,
     {
         path: "*",
         element: <Navigate to="/404" />,
